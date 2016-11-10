70723ed9e55374a097dd12528be476dbd10c7b6c
Kejnnary <kejnnary@mail.ru>
2016-11-10 15:44:11 +0200

    Add class Aeroflot

---
+++ 02-Task-Aeroflot/02-Task-Aeroflot/Aeroflot.swift
@@ -0,0 +1,21 @@
+//
+//  Aeroflot.swift
+//  02-Task-Aeroflot
+//
+//  Created by user on 10.11.16.
+//  Copyright © 2016 user. All rights reserved.
+//
+
+import Foundation
+
+class Aeroflot {
+    var nameOfFlight: String    //название пункта назначения
+    var numberOfFlight: Int     //номер рейса
+    var typeOfAircraft: String  //тип самолета
+    
+    init(nameOfFlight: String, numberOfFlight: Int, typeOfAircraft: String) {
+        self.nameOfFlight = nameOfFlight
+        self.numberOfFlight = numberOfFlight
+        self.typeOfAircraft = typeOfAircraft
+    }
+}
--- 02-Task-Aeroflot/02-Task-Aeroflot/main.swift
+++ 02-Task-Aeroflot/02-Task-Aeroflot/main.swift
@@ -14,3 +14,21 @@ import Foundation
  упорядочить массив по убыванию номера рейса;
  вывод номер рейсов и типов самолетов, вылетающих в пункт, название которого совпало с названием, введенным пользователем.
 */
+
+print("1 - entering information")
+print("2 - sort by number")
+print("3 - find flight")
+
+var yourChoice = readLine()!
+switch yourChoice {
+    case "1":
+        print("your choice is 1")
+    case "2":
+        print("your choice is 2")
+    case "3":
+        print("your choice is 3")
+    default:
+        print("not good choice")
+}
+
+
